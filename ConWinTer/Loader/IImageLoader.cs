517f0759c042a155cf07b44c29cad9db1ddfbdb9
Martin Vitek <martulavitek@seznam.cz>
2021-02-14 10:49:48 +0100

    add comments to IImageLoader

--- ConWinTer/Loader/IImageLoader.cs
+++ ConWinTer/Loader/IImageLoader.cs
@@ -5,10 +5,24 @@ using System.Drawing;
 
 namespace ConWinTer.Loader {
     public interface IImageLoader {
-        public abstract Image FromFile(string path);
+        /// <summary>
+        /// Loads image from file specified by <paramref name="path"/> as a bitmap
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public Image FromFile(string path);
 
+        /// <summary>
+        /// Returns true if file extension is supported
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         public bool IsSupportedFile(string path);
 
+        /// <summary>
+        /// Returns all supported extensions
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<string> GetSupportedExtensions();
     }
 }
