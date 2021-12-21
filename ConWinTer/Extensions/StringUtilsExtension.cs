1d00ee6f11427be3ee052d8fa8857c0a34f540b9
Martin Vitek <martulavitek@seznam.cz>
2021-12-21 13:33:27 +0100

    fix empty email address check

--- ConWinTer/Extensions/StringUtilsExtension.cs
+++ ConWinTer/Extensions/StringUtilsExtension.cs
@@ -6,6 +6,9 @@ using System.Text;
 namespace ConWinTer.Extensions {
     public static class StringUtilsExtension {
         public static bool IsValidEmailAddress(this string s) {
+            if (string.IsNullOrWhiteSpace(s))
+                return false;
+
             try {
                 var m = new MailAddress(s);
                 return true;
