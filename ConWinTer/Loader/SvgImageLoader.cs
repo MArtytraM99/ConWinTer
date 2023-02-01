49124926f33c25bf4f93aa20a6fb34188aaebc3a
Martin Vitek <vitekma6@fit.cvut.cz>
2023-02-01 16:50:20 +0100

    use specified colors for fill, stroke and color when set

--- ConWinTer/Loader/SvgImageLoader.cs
+++ ConWinTer/Loader/SvgImageLoader.cs
@@ -9,27 +9,27 @@ using System.Text;
 
 namespace ConWinTer.Loader {
     public class SvgImageLoader : IImageLoader {
-        private readonly SvgPaintServer svgPaintServer;
+        private readonly SvgPaintServer defaultPaintServer;
 
         public SvgImageLoader() : this(Color.Black) {}
 
-        public SvgImageLoader(Color color) {
-            svgPaintServer = new SvgColourServer(color);
+        public SvgImageLoader(Color defaultColor) {
+            defaultPaintServer = new SvgColourServer(defaultColor);
         }
 
         public Image FromFile(string path) {
             var svgDoc = SvgDocument.Open<SvgDocument>(path);
 
-            processNodes(svgDoc.Descendants(), svgPaintServer);
+            processNodes(svgDoc.Descendants(), defaultPaintServer);
 
             return svgDoc.Draw();
         }
 
         private void processNodes(IEnumerable<SvgElement> nodes, SvgPaintServer colorServer) {
             foreach (var node in nodes) {
-                if (node.Fill != SvgPaintServer.None) node.Fill = colorServer;
-                if (node.Color != SvgPaintServer.None) node.Color = colorServer;
-                if (node.Stroke != SvgPaintServer.None) node.Stroke = colorServer;
+                if (!node.ContainsAttribute("fill")) node.Fill = colorServer;
+                if (!node.ContainsAttribute("stroke")) node.Stroke = node.Fill;  // use node.Fill color if it `fill` is set and `stroke` is not
+                if (!node.ContainsAttribute("color")) node.Color = colorServer;
 
                 processNodes(node.Descendants(), colorServer);
             }
